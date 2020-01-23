20e7ef850b3862cfe47eef2535b0f86a574a0216
kensadera <43423520+kensadera@users.noreply.github.com>
2020-01-23 14:05:04 +0300

    InitialCommit

---
+++ RenataApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+
+using Microsoft.EntityFrameworkCore;
+using Renata.API.Models;
+
+namespace Renata.API.Data
+{
+    public class DataContext : DbContext
+    {
+    	public DataContext(DbContextOptions<DataContext> options): base(options){}
+						
+		public DbSet <Value> Values{get; set;}
+
+    }
+}
